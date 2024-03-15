a6233ffddc369f1ee9a22c99992f99c417c1793b
SammriddhGupta <sammriddhgupta@gmail.com>
2024-03-16 00:28:00 +1100

    feat: update to use embeddings and LLM from HuggingFace

--- pages/2_pdf_chat_hf.py
+++ pages/2_pdf_chat_hf.py
@@ -12,47 +12,46 @@ from htmlTemplates import *
 # Loop through all the pdfs, loop through all the pages, extract the text and return it as a string
 def get_pdf_text(pdf_docs):
     text = ""
-    for pdf in pdf_docs:
+    for pdf in pdf_docs: 
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
-    
     return text
-    
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
+        separator="\n", # split by new line
+        chunk_size=1000, # split into chunks of 1000 characters
+        chunk_overlap=200, # overlap chunks by 200 characters
+        length_function=len 
     )
-    chunks = text_splitter.split_text(text)
+    chunks = text_splitter.split_text(text) # split the text into chunks
     return chunks
 
 def get_vectorstore(text_chunks, api_key):
-    embeddings = OpenAIEmbeddings(openai_api_key=api_key, model="text-embedding-ada-002")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
     return vectorstore
 
 def get_conversation_chain(vectorstore, api_key):
-    my_llm = ChatOpenAI(openai_api_key=api_key, model="gpt-3.5-turbo")
-    
+    my_llm = HuggingFaceHub(huggingfacehub_api_token=api_key, repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    # now we need to initialise an instance of memory
     my_memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=my_llm, retriever=vectorstore.as_retriever(), memory=my_memory)
-    
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=my_llm, retriever=vectorstore.as_retriever(),memory=my_memory
+    )
     return conversation_chain
     
 def handle_user_input(user_question):
-   response = st.session_state.conversation.invoke({'question':user_question})
-   
-   st.session_state.chat_history = response['chat_history']
-   
-   for i, msg in enumerate(st.session_state.chat_history):
-       if i % 2 == 0:
-           st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
-       else:
+    response = st.session_state.conversation.invoke({'question': user_question})
+    # st.write(response) # for debugging
+    
+    # now creating a new session state for our chat history
+    st.session_state.chat_history = response['chat_history']
+    
+    for i, msg in enumerate(st.session_state.chat_history):
+        if i % 2 ==0:
+            st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
+        else:
             st.write(bot_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
 
 def main():
