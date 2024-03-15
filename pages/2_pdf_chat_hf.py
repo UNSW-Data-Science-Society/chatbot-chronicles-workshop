6785a909bc7d554ff7f4eb552b65a85b58ea059b
SammriddhGupta <sammriddhgupta@gmail.com>
2024-03-16 00:26:07 +1100

    feat: add logic for pdf chat

--- pages/2_pdf_chat_hf.py
+++ pages/2_pdf_chat_hf.py
@@ -11,24 +11,49 @@ from htmlTemplates import *
 
 # Loop through all the pdfs, loop through all the pages, extract the text and return it as a string
 def get_pdf_text(pdf_docs):
-    #todo
-    return ""
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    
+    return text
+    
 
 def get_text_chunks(text):
-    #todo
-    return ""
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def get_vectorstore(text_chunks, api_key):
-    #todo
-    return ""
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key, model="text-embedding-ada-002")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 def get_conversation_chain(vectorstore, api_key):
-    #todo
-    return ""
+    my_llm = ChatOpenAI(openai_api_key=api_key, model="gpt-3.5-turbo")
+    
+    my_memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=my_llm, retriever=vectorstore.as_retriever(), memory=my_memory)
+    
+    return conversation_chain
     
 def handle_user_input(user_question):
-    #todo
-    return ""
+   response = st.session_state.conversation.invoke({'question':user_question})
+   
+   st.session_state.chat_history = response['chat_history']
+   
+   for i, msg in enumerate(st.session_state.chat_history):
+       if i % 2 == 0:
+           st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
+       else:
+            st.write(bot_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
 
 def main():
     st.set_page_config(page_title='Go chat with pdfs', page_icon=':robot_face:')
